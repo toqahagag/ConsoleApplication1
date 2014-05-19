4b09a1a03b3abcbc799e57d83d3f4eeeeaf0bb89
basma-zeyada <basma_zeyada@live.com>
2014-05-19 21:31:48 +0300

    commited

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a");
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter b");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            string sa = a.ToString();
+            int ia=sa.IndexOf("0");
+           int newa= int.Parse(sa.Remove(ia, 1));
+            string sb = b.ToString();
+            int ib = sb.IndexOf("0");
+           int newb=int.Parse( sb.Remove(ib, 1));
+           string sc = c.ToString();
+           int ic = sa.IndexOf("0");
+           int newc = int.Parse(sc.Remove(ic, 1));
+           int newc2 = newa + newb;
+           if (newc == newc2)
+           {
+               Console.WriteLine("true");
+           }
+           else
+           {
+               Console.WriteLine("false");
+           }
+           Console.ReadLine();
+        }
+        
+    }
+    
+}
---
+++ ConsoleApplication1/ConsoleApplication3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter word");
+            string w = Console.ReadLine();
+           string a= w.ToLower();
+           string firstletter = a.Substring(0, 1).ToUpper();
+           string sub=a.Remove(0, 1);
+           string newword = firstletter +sub;
+           Console.WriteLine(newword);
+           Console.ReadLine();
+        }
+    }
+}
